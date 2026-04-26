76d4d49be2fff989de24593a9e273e638354219f
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-26 23:45:16 +0530

    setting controllers

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 
 const PORT = process.env.PORT || 5000
 
-const app = express()
+// const app = express()
 
-// app.get("/", (req, res) => {
-//     res.json({
-//         message: "Hello World"
-//     })
-// })
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello World"
+    })
+})
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
