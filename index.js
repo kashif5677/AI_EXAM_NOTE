841f9454e5b8a9dc31db7391d2f2cceb3dbc3b3f
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-26 23:41:40 +0530

    AI exam notes

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello World"
+    })
+})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
