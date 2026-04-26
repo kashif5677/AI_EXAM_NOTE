ac98883755d187d748d2882ca1fc245b5b2ace6b
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-26 23:42:36 +0530

    adding server

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get("/", (req, res) => {
         message: "Hello World"
     })
 })
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`)
+// })
