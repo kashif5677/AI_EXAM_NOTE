44d3eb7e2af9c337894c1ad9032c1d1c98908683
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-29 23:51:37 +0530

    update

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -5,7 +5,7 @@ function Auth() {
   return (
     <div className='min-h-screen overflow-hidden bg-white text-black px-8'>
       <motion.header 
-      initial={{opacity:0,y:-15}}
+    //   initial={{opacity:0,y:-15}}
       animate={{opacity:1,y:0}}
       transition={{duration:0.5}}
         className='max-w-7xl mx-auto mt-8
