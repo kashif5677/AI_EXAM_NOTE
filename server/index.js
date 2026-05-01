a24914931a4552e07de80f29ffef731d4a5907c5
mdkashif <mdkashifnisar5677@gmail.com>
2026-05-01 22:48:22 +0530

    file added

---
+++ server/index.js
@@ -0,0 +1,12 @@
+﻿import express from "express";
+
+const port = 8000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World!' })
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
