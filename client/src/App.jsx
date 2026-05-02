89cab9799dc4beae9607e8389feff985f9eef3b6
mdkashif <mdkashifnisar5677@gmail.com>
2026-05-02 12:35:07 +0530

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
-
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Auth from "./pages/Auth"
 
 function App() {
  
   return (
   <>
-    <div>
-      hello
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
   </>
   )
 }
