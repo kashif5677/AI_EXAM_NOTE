cbfd7fdd66c959603d20f00326ac120f279e9ae6
mdkashif <mdkashifnisar5677@gmail.com>
2026-05-04 23:56:04 +0530

    adding div

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -56,9 +56,9 @@ function Auth() {
 
         </motion.div>
         {/* Right content */}
-        <motion.div className='grid grid-cols-1 sm:grid-cols-2 gap-8'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-8'>
             <Feature icon="🎁" title="50 Free Credits" desc="Start with 50 credits to generate notes without paying."/>
-        </motion.div>
+        </div>
 
       </main>
     </div>  
