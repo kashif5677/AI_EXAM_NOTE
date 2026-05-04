4d24f0b11c9024740b523ae91f909092db99f39d
mdkashif <mdkashifnisar5677@gmail.com>
2026-05-04 13:00:13 +0530

    fetures function

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -36,10 +36,23 @@ function Auth() {
         Unlock Smart <br /> AI Notes
         </h1>
         <motion.button
+            whileHover={{
+                y:-10,
+                rotateX:8,
+                rotateY:-8,
+                scale:1.07
+            }} 
+            whileTap={{scale:0.97}}
+            transition={{type:"spring",stiffness:200,damping:18}}
             className='mt-10 px-10 py-3 rounded-xl flex items-center gap-3 bg-gradient-to-br from-black/90 via-black/80 to-black/90 border border-white/10 text-white font-semibold text-lg shadow-[0_25px_60px_rgba(0,0,0,0.7)]'
         >
-            <FcGoogle/>
+            <FcGoogle size={22}/>
+            Continue with Google
         </motion.button>
+        <p className='mt-6 max-w-xl text-lg bg-gradient-to-br from-gray-700 via-gray-500/80 to-gray-700 bg-clip-text text-transparent'>
+            You get <span className='font-semibold'>50 Free credits</span> to create exam notes,project notes,charts,graphs and download clean PDFs - instantly using AI.
+        </p>
+        <p className='mt-4 text-sm'>Start with 50 free credits -Upgrade anytime for more credits -Instant access </p>
 
         </motion.div>
         {/* Right content */}
@@ -48,6 +61,25 @@ function Auth() {
         </motion.div>
       </main>
     </div>
+  )
+}
+function Feature({icon,title,des}){
+  return(
+    <motion.div
+    whileHover={{y:-12,rotateX:8,rotateY:-8,scale:1.07}}
+    transition={{type:"spring",stiffness:200,damping:18}}
+    className='relative rounded-2xl p-6 bg-gradient-to-br from-black/90 via-black/80 to-black/90 backdrop-blur-2xl border border-white/10 shadow-[0_30px_80px_rbga(0,0,0,0.7)]
+    text-white' 
+      style={{transformStyle:"preserve-3d"}}
+    >
+      <div className='absolute inset-0 rounded-2xl bg-gradient-to-br from-white/10 to-transparent opacity-0 hover:opacity-100 transition-opacity pointer-events-none'>
+          <div className='relative z-10' style={{transform:"translateZ(30px)"}}>
+            <div className='text-4xl mb-3'>{icon}</div>
+            <h3 className='text-lg font-semibold mb-2'>{title}</h3>
+            <p className='text-gray-300 text-sm leading-relaxed'>{des}</p>
+          </div>
+      </div>
+    </motion.div>
   )
 }
 
