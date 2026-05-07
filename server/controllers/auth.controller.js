b5ab4f302dada1a9f61c4365d1b4d64b16a837c9
mdkashif <mdkashifnisar5677@gmail.com>
2026-05-07 22:58:59 +0530

    token

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -17,7 +17,7 @@ export const googleAuth = async (req, res) => {
         })
         return res.status(200).json(user)
     } catch (error) {
-        return res.status(500).json({ message: error.message })
+        // return res.status(500).json({ message: error.message })
     }
 }
 
