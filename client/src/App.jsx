4cfaee69533922ef36cba383a68259db5af9d888
mdkashif <mdkashifnisar5677@gmail.com>
2026-05-08 23:29:18 +0530

    getcurrentuser

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   <>
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/auth" element={<Auth />} /> */}
+      <Route path="/auth" element={<Auth />} />
     </Routes>
   </>
   )
