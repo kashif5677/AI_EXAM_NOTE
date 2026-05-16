5f883c92e9cb1ec65c47b971276d1d0cc4298025
mdkashif <mdkashifnisar5677@gmail.com>
2026-05-16 23:12:36 +0530

    notes data

--- client/src/pages/Notes.jsx
+++ client/src/pages/Notes.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 function Notes() {
   return (
-    <div>
-      
+    <div className='min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 px-6 py-8'> 
+       <motion.header
+      initial={{y:-15,opacity:0}}
+      animate={{y:0,opacity:1}}
+      transition={{duration:2}}
+
+      className='max-w-6xl mx-auto mt-8 
+      rounded-2xl bg-black/80 backdrop-blur-xl 
+      border border-white/10
+      px-8 py-6
+      shadow-[0_20px_45px_rgba(0,0,0,0.6)]
+      '>
+        <h1 className='text-2xl font-bold bg-linear-to-r from-white via-gray-400 to-white bg-clip-text text-transparent'>
+            ExamNotes AI
+        </h1>
+        <p className='text-sm text-gray-400 mt-1'>
+            AI-powered exam-oriented nots & revision
+        </p>
+
+      </motion.header>
     </div>
   )
 }
