2385ec6b3404bd678e21481ff904926da95686bc
mdkashif <mdkashifnisar5677@gmail.com>
2026-05-17 23:25:09 +0530

    note header

--- client/src/pages/Notes.jsx
+++ client/src/pages/Notes.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-
+import { useNavigate } from 'react-router-dom'
 function Notes() {
+  const navigate=useNavigate()
   return (
     <div className='min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 px-6 py-8'> 
        <motion.header
@@ -9,18 +10,21 @@ function Notes() {
       animate={{y:0,opacity:1}}
       transition={{duration:2}}
 
-      className='max-w-6xl mx-auto mt-8 
+      className='mb-10 
       rounded-2xl bg-black/80 backdrop-blur-xl 
       border border-white/10
       px-8 py-6
-      shadow-[0_20px_45px_rgba(0,0,0,0.6)]
+      shadow-[0_20px_45px_rgba(0,0,0,0.6)] items-start
+      flex md:items-center justify-between gap-4 flex-col md:flex-row
       '>
-        <h1 className='text-2xl font-bold bg-linear-to-r from-white via-gray-400 to-white bg-clip-text text-transparent'>
+        <div onClick={()=>navigate("/")} className='cursor-pointer'>
+              <h1 className='text-2xl font-bold bg-linear-to-r from-white via-gray-400 to-white bg-clip-text text-transparent'>
             ExamNotes AI
         </h1>
         <p className='text-sm text-gray-400 mt-1'>
-            {/* AI-powered exam-oriented nots & revision */}
+            AI-powered exam-oriented nots & revision
         </p>
+        </div>
 
       </motion.header>
     </div>
