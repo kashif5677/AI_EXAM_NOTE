03f735376a8dde44c36b2f722a5a3542f7038df0
mdkashif <mdkashifnisar5677@gmail.com>
2026-06-03 02:39:28 +0530

    render

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://ai-exam-note-server.onrender.com',
         changeOrigin: true,
         secure: false,
       }
--- server/index.js
+++ server/index.js
@@ -17,14 +17,15 @@ dotenv.config()
 const app = express()
 
 app.post(
-  "/api/credits/webhook",
-  express.raw({ type: "application/json" }),
-  stripeWebhook
+    "/api/credits/webhook",
+    express.raw({ type: "application/json" }),
+    stripeWebhook
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
-        credentials:true,
+    {
+        origin: "https://ai-exam-note-client.onrender.com",
+        credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
 ))
@@ -34,19 +35,19 @@ app.use(cors(
 app.use(express.json())
 app.use(cookieParser())
 const PORT = process.env.PORT || 5000
-app.get("/",(req,res)=>{
-    res.json({message:"ExamNotes AI Backend Running 🚀"})
+app.get("/", (req, res) => {
+    res.json({ message: "ExamNotes AI Backend Running 🚀" })
 
 })
-app.use("/api/auth" , authRouter)
+app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/notes", notesRouter)
 app.use("/api/pdf", pdfRouter)
-app.use("/api/credit",creditRouter)
+app.use("/api/credit", creditRouter)
 
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`✅ Server running on port ${PORT}`)
     connectDb()
 })
